a78b553096dce8d31a0a977e68bdfef77720baf8
Aleksey Alkhutov <alexey.alkhutov@dunice.ru>
2017-04-05 15:22:28 +0300

    show top10

--- app.py
+++ app.py
@@ -23,12 +23,13 @@ from string import maketrans
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 from nltk.util import ngrams
+# from sklearn.feature_extraction.text import CountVectorizer
 
 def word_grams(text, n):
-    print('text',text)
+    # print('text',text)
     s = []
     for ngram in ngrams(text, n):
-        print('ngram', ngram)
+        # print('ngram', ngram)
         s.append(tuple(ngram))
     return s
 
@@ -39,22 +40,22 @@ def process_file(path, n):
     @n is number of n-grams
     """
     print word_grams('one two three four'.split(' '), n)
-    # with open('text.txt', 'r') as sample_file:
-    #     for line in sample_file:
-    #         print('*', [i for i in word_tokenize(line.lower()) if i not in stop])
     print('Start processing...')
     try:
         sample_file = open(path, 'r')
         read_file = sample_file.read()
-        print(read_file)
         stop = string.punctuation + '\n'
-        print(stop)
         file_content = re.split(r'\s+|[,;:.-]\s*|\\n', read_file)
-        print(file_content)
-        print('Result:{}'.format(word_grams(file_content, n)))
+        # print(file_content)
+        fdist = nltk.FreqDist( word_grams(file_content, n) )
+        top10 = sorted(fdist.items(), key=lambda x: x[1], reverse=True)[:10]
+        print('TOP10 result:')
+        for k,v in top10:
+            print k,v
     except (IOError, TypeError) as ex:
         print('FILE ERROR! {}'.format(ex))
         print('You may forgot -p flag with value')
+
 
 def main(argv):
     ngrams = 2
