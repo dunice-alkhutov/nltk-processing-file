f39143cdb4f83b436ed2e09a92471f4fa48b7930
Aleksey Alkhutov <alexey.alkhutov@dunice.ru>
2017-04-07 11:28:46 +0300

    add script to create random sample file

---
+++ create_random_sample.py
@@ -0,0 +1,17 @@
+import random
+
+rnd_lines = []
+while len(rnd_lines) < 100:
+    rnd = random.randint(1, 612000)
+    if rnd not in rnd_lines:
+        rnd_lines.append(rnd)
+
+print(rnd_lines)
+
+dst = open('dst.txt', 'w')
+
+with open("dump 2.txt") as fp:
+    for i, line in enumerate(fp):
+        if i in rnd_lines:
+            dst.write(line)
+dst.close
