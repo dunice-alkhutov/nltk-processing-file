48d28af0a089ee1edfa2b1ce531979fc6bf58f34
Aleksey Alkhutov <alexey.alkhutov@dunice.ru>
2017-04-07 15:58:44 +0300

    update create random sample script

--- create_random_sample.py
+++ create_random_sample.py
@@ -1,15 +1,53 @@
 import random
+import sys, getopt
 
-rnd_lines = []
-while len(rnd_lines) < 100:
-    rnd = random.randint(1, 612000)
-    if rnd not in rnd_lines:
-        rnd_lines.append(rnd)
+def main(argv):
+    source_file = None
+    destination_file = None
+    num_lines = 100
+    try:
+        opts, _ = getopt.getopt(argv, "s:d:n:h", ["source=", "dest=", "help="])
+    except getopt.GetoptError as ex:
+        print(ex)
+        print("See help by 'python app.py -h'")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            message = '''You can use next flags:
+            -s  path to source file(required)
+            -d  path to destination file(required)
+            -n  number of lines to put into destination file
+            Example: "python app.py -s /path/to/file1 -d /path/to/file2'''
+            print(message)
+        elif opt == '-s':
+            source_file = arg
+        elif opt == '-d':
+            destination_file = arg
+        elif opt == '-n':
+            num_lines = int(arg)
+        else:
+            sys.exit()
 
-dst = open('dst.txt', 'w')
+    if not source_file:
+        print('Source file is missed')
+        sys.exit()
+    if not destination_file:
+        print('Destination file is missed')
+        sys.exit()
 
-with open("dump 2.txt") as fp:
-    for i, line in enumerate(fp):
-        if i in rnd_lines:
-            dst.write(line)
-dst.close
+    rnd_lines = []
+    while len(rnd_lines) < num_lines:
+        rnd = random.randint(1, 612000)
+        if rnd not in rnd_lines:
+            rnd_lines.append(rnd)
+
+    dst = open(destination_file, 'w')
+
+    with open(source_file) as fp:
+        for i, line in enumerate(fp):
+            if i in rnd_lines:
+                dst.write(line)
+    dst.close
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
